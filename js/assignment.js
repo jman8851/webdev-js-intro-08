b0e16a976fa8cee6d66de5460e03e764f9f968da
Jason Caldwell <jman8851@gmail.com>
2024-12-30 22:36:54 -0500

    updated Readme and index code experiement. commented out all index code and compiled working code in assignment.js per instructions and tested code by clicking submit button.

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,37 @@ const largestNumberElement = document.getElementById("largest-number");
 const averageNumberElement = document.getElementById("average-number");
 
 const myNumbers = [35, 130, 354, 100, 25, 134, 99, 200, 35];
+
+
+function findSmallestNumber(arr) { 
+    let smallest = arr[0]; 
+    for(let i = 1; i < arr.length; i++) { 
+        if(arr[i] < smallest) { 
+            smallest = arr[i]; 
+        } 
+    } 
+    return smallest; 
+}
+
+
+function findLargestNumber(arr) { 
+    let largest = arr[0]; 
+    for(let i = 1; i < arr.length; i++) { 
+        if(arr[i] > largest) { largest = arr[i]; 
+        } 
+    } return largest; 
+}
+
+function findAverage(arr) {
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+
+submissionBtn.addEventListener("click", function() {
+    smallestNumberElement.textContent = findSmallestNumber(myNumbers);
+    largestNumberElement.textContent = findLargestNumber(myNumbers);
+    averageNumberElement.textContent = findAverage(myNumbers);
+});
--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*let grades = [85, 90, 78, 92, 88];
+let sum = 0;
+for (let i = 0; i < grades.length; i++) {
+    sum += grades[i];
+}
+
+let average = sum / grades.length;
+console.log(average);*/
